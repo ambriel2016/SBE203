92a89515ddfdf3428c11e1134c811ab5fe94ea1f
GBTC440002UR <GBTC440002UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-17 16:14:44 -0500

    SBE203

---
+++ src/main/java/com/cristian/demo/employee.java
@@ -0,0 +1,40 @@
+package com.cristian.demo;
+
+public class employee {
+    private String firstName;
+    private String lastName;
+    private int SSN;
+    private int DOB;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(int SSN) {
+        this.SSN = SSN;
+    }
+
+    public int getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(int DOB) {
+        this.DOB = DOB;
+    }
+}
